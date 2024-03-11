49e6d6b328b9caba53af361457487a266d882c75
Kenza-ighmouracene <kenza.ighmouracene@alumni.univ-avignon.fr>
2024-03-11 13:42:31 +0100

    fonctions de conversion

---
+++ src/main/java/fonction/VitesseConv.java
@@ -0,0 +1,44 @@
+
+
+
+public class VitesseConv {
+
+    public static double convert(String unitFrom, String unitTo, double value) {
+        // Conversion des vitesses en mètres par seconde
+        if ("m/s".equals(unitFrom)) {
+            if ("km/h".equals(unitTo)) {
+                return value * 3.6;
+            } else if ("mph".equals(unitTo)) {
+                return value * 2.23694;
+            } else if ("noeuds".equals(unitTo)) {
+                return value * 1.94384;
+            }
+        } else if ("km/h".equals(unitFrom)) {
+            if ("m/s".equals(unitTo)) {
+                return value / 3.6;
+            } else if ("mph".equals(unitTo)) {
+                return value / 1.60934;
+            } else if ("noeuds".equals(unitTo)) {
+                return value / 1.852;
+            }
+        } else if ("mph".equals(unitFrom)) {
+            if ("m/s".equals(unitTo)) {
+                return value / 2.23694;
+            } else if ("km/h".equals(unitTo)) {
+                return value * 1.60934;
+            } else if ("noeuds".equals(unitTo)) {
+                return value / 1.15078;
+            }
+        } else if ("noeuds".equals(unitFrom)) {
+            if ("m/s".equals(unitTo)) {
+                return value / 1.94384;
+            } else if ("km/h".equals(unitTo)) {
+                return value * 1.852;
+            } else if ("mph".equals(unitTo)) {
+                return value * 1.15078;
+            }
+        }
+
+        throw new IllegalArgumentException("Conversion non supportée");
+    }
+}
