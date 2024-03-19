ccab4df1e11f7b60735334536b16679a00caeab5
Kenza-ighmouracene <kenza.ighmouracene@alumni.univ-avignon.fr>
2024-03-20 00:00:34 +0100

    fonctions de conversion list de list

--- src/main/java/fonction/ConvTemperature.java
+++ src/main/java/fonction/ConvTemperature.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ConvTemperature {
+    //conversion
     public static List<List<Double>> convertir(String uniteDepart, List<String> unitesArrivee, List<Double> valeurs) {
         List<List<Double>> resultats = new ArrayList<>();
 
